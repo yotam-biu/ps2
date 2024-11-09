43926a9d3f9014070a584c627f6ba32a1195a611
yotam-biu <nagaryo3@biu.ac.il>
2024-11-09 11:37:38 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,21 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+
+    return max(num1, num2, num3)
+    
+    # pass  # Replace 'pass' with code
 
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+
+    return (num1 + num2 + num3)/3
+    
+    # pass  # Replace 'pass' with code
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+
+    variance = (num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2
+    variance /= 3 # Complete this line to calculate variance
+    return mean, variance**0.5  # Standard deviation is the square root of variance
+    
+    #pass  # Replace 'pass' with code
 
